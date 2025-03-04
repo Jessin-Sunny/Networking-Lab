903e5aa94e81eda00f73b0867c6951b74f5eec1b
Jessin Sunny <119120870+Jessin-Sunny@users.noreply.github.com>
2025-03-04 21:18:38 +0530

    Update Client.c

--- Multi-User TCP/Client.c
+++ Multi-User TCP/Client.c
@@ -1,57 +1,83 @@
 #include <stdio.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
+#include <stdlib.h>
 #include <string.h>
-#include <arpa/inet.h>
 #include <unistd.h>
+#include <pthread.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+int client;
+
+void *recieve_msg(void *arg)
+{
+    char buffer[1024];
+    int valread;
+    while(1)
+    {
+        memset(buffer, 0, sizeof(buffer));
+        valread = recv(client, buffer, sizeof(buffer), 0);
+        if(valread <= 0)
+        {
+            printf("Disconnected\n");
+            exit(0);
+        }
+        printf("%s", buffer);
+    }
+}
 
 int main()
 {
-    int client;
-    char buffer[1024];
+    char username[50], buffer[1024];
     struct sockaddr_in servAddr;
 
-    client = socket(AF_INET, SOCK_STREAM, 0);
-    servAddr.sin_family = AF_INET;
-    servAddr.sin_port = htons(2136);
-    servAddr.sin_addr.s_addr = inet_addr("127.0.36");
 
-    if(connect(client,(struct sockaddr *)&servAddr,sizeof(servAddr)) <0)
+    client = socket(AF_INET, SOCK_STREAM, 0);
+    if(client < 0)
     {
-        perror("Connection Failed");
+        printf("Socket creation failed\n");
         return -1;
     }
+    else
+    {
+        printf("Socket creation successfull\n");
+    }
 
-    printf("Connected to server \n");
+    servAddr.sin_family = AF_INET;
+    servAddr.sin_port = htons(3621);
+    servAddr.sin_addr.s_addr = inet_addr("127.0.0.36");
+    socklen_t size = sizeof(servAddr);
+
+    if(connect(client, (struct sockaddr *)&servAddr, size ) < 0)
+    {
+        printf("Connection failed\n");
+        return -1;
+    }
+     else
+    {
+        printf("Connection successfull\n");
+    }
+
+    printf("Enter your username : ");
+    fgets(username, sizeof(username), stdin);
+    username[strcspn(username, "\n")] = '\0';
+
+    send(client, username, strlen(username), 0 );
+
+    pthread_t recievethread;
+    pthread_create(&recievethread, NULL, recieve_msg, NULL);
+    pthread_detach(recievethread);
+
+    printf("Type the Message\n");
 
     while(1)
     {
-        memset(buffer,0,sizeof(buffer));
-        printf("Enter a message : ");
-        fgets(buffer,sizeof(buffer),stdin);
-        buffer[strcspn(buffer,"\n")] = '\0';
-
+        memset(buffer, 0, sizeof(buffer));
+        fgets(buffer, sizeof(buffer), stdin);
         send(client, buffer, strlen(buffer), 0);
-
-        if(strcmp(buffer,"exit") == 0)
-        {
-            printf("Exiting client...\n");
-            break;
-        }
-
-        int valread = recv(client, buffer, sizeof(buffer) - 1, 0);
-	if(valread > 0)
-	{
-	    buffer[valread] = '\0';
-	    printf("Server: %s\n", buffer);
-	}
-
-	if(strcmp(buffer,"exit") == 0)
-	{
-	    printf("Server exited...Closing client...\n");
-	    break;
-	}
     }
+
     close(client);
     return 0;
+
 }
